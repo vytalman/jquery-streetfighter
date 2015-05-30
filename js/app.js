f571801976c349d61b2f77f900b1df0e9670bd96
vytalman <ryanmesa@gmail.com>
2015-05-30 19:28:56 -0400

    Fix mouse functions

--- js/app.js
+++ js/app.js
@@ -1,21 +1,25 @@
 $(document).ready(function () {
 	$('.ryu').mouseenter(function() {
-		/*alert('mouse entered .ryu div');*/
+		//alert('mouse entered .ryu div');
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
 	})
 	.mouseleave(function() {
-		/*alert('mouse left .ryu div');*/
+		//alert('mouse left .ryu div');
 		$('.ryu-still').show();
 		$('.ryu-ready').hide();
 	})
 	.mousedown(function() {
-		console.log('mousedown');
+		//console.log('mousedown');
 		// play hadouken sound
-		// show hadouken and animate it to the right of the screen
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
+		// animate hadouken to the right of the screen
 	})
 	.mouseup(function() {
-		console.log('mouseup');
-		// ryu goes back to his ready position
+		//console.log('mouseup');
+		$('.ryu-throwin').hide();
+		$('.ryu-ready').show();
 	});
 });
