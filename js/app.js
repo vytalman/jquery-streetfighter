47c382ad9beb736de39010e2b7dfb787cb7b5efb
vytalman <ryanmesa@gmail.com>
2015-06-01 23:46:30 -0400

    add hadouken

--- js/app.js
+++ js/app.js
@@ -12,10 +12,16 @@ $(document).ready(function () {
 	.mousedown(function() {
 		//console.log('mousedown');
 		// play hadouken sound
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		// animate hadouken to the right of the screen
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'}, 500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '570px');
+			}
+		);
 	})
 	.mouseup(function() {
 		//console.log('mouseup');
@@ -23,3 +29,9 @@ $(document).ready(function () {
 		$('.ryu-ready').show();
 	});
 });
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
